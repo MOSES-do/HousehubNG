4523a836f29806b92e076eb9e93a8f0b91f3f65d
Esumei Moses <archer.me24@gmail.com>
2024-11-02 13:27:43 +0100

    persist data on tab switch of same page

---
+++ frontend/src/components/PageReload.js
@@ -0,0 +1,37 @@
+import fetchApi from "./Search.js";
+import renderHouseList from "./HouseList.js";
+import { state, resultLength } from "../common.js";
+
+
+export default async function handlePageByHash(currentPage) {
+    const hashValue = window.location.hash.substring(1);
+    if (window.location.hash) {
+        if (hashValue === currentPage) {
+            try {
+                const urlParams = new URLSearchParams(window.location.search);
+                // Get the value of the 'search' parameter
+                const searchQuery = urlParams.get('search');
+                const response = await fetchApi(searchQuery);
+                if (response.ok) {
+                    resultLength.textContent = state.searchHouseItems.length
+                    renderHouseList();
+                } else {
+                    const errorData = await response.json();
+                    alert('Registration failed: ' + errorData.error)
+                }
+
+            } catch (error) {
+            }
+        }
+    } else {
+        // Optionally, handle the default case if there's no hash
+        navigateTo('home'); // Default to "home" or any other default page
+    }
+}
+
+
+
+// we can also check for hashchange where the functiin is being called
+// window.addEventListener('hashchange', async () => {
+//     await handlePageByHash('product_list'); // Update based on the new hash
+// });
