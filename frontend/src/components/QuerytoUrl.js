3fb6ace7cbed6206ffeb4be43c8ef363802fc027
Esumei Moses <archer.me24@gmail.com>
2024-11-02 13:28:30 +0100

    add search query to url

---
+++ frontend/src/components/QuerytoUrl.js
@@ -0,0 +1,20 @@
+
+/**
+ * Add query parameter to the url for easy retrieval
+ */
+
+export function urlUpdate(searchInput, customPath) {
+    const query = searchInput.trim();
+    if (query) {
+        // URLSearchParams object with current URL params
+        console.log(query)
+        const urlParams = new URLSearchParams(window.location.search)
+
+        // Update the "search" parameter with user's query
+        urlParams.set('search', query);
+
+        // Update the URL without reloading the page
+        const newUrl = `/?${urlParams.toString()}#${customPath}`;
+        window.history.pushState({ path: newUrl }, '', newUrl);
+    }
+}
