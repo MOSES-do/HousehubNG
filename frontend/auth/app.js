52d82981e3efa94b8838689fe1100ddffda177f9
Esumei Moses <archer.me24@gmail.com>
2025-01-18 17:43:58 +0100

    active tab on reload updated

--- frontend/auth/app.js
+++ frontend/auth/app.js
@@ -134,20 +134,16 @@ document.querySelector('.g-auth').addEventListener('click', function () {
         });
 });
 
-window.onload = function () {
-
-
-    // Automatically prompt user authentication
-};
 
 window.addEventListener('load', () => {
     let oauthCode;
 
+    // Automatically prompt user authentication
     google.accounts.id.initialize({
         client_id: "119453756942-20vc1f6u1fdi4bdv0upidob2s14am44q.apps.googleusercontent.com",
         callback: (response) => {
             oauthCode = response.credential;
-            console.log('oauthCode');
+            console.log(oauthCode);
         }
     })
     if (window.location.hash === '#home')
