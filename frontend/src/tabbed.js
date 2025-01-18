073dfa27f483df05eaf9a12d79bf096f619b0417
Esumei Moses <archer.me24@gmail.com>
2025-01-18 16:45:43 +0100

    active tab on reload updated

--- frontend/src/tabbed.js
+++ frontend/src/tabbed.js
@@ -206,7 +206,7 @@ const tab = document.querySelector('.dashboard_tiles .tab');
 
 
 /**===Refactor===**/
-let lastScrollY = 10;
+let lastScrollY = 5;
 let isScrollingUp = false;
 // Throttle function to limit how often the scroll handler runs
 function throttle(callback, limit) {
